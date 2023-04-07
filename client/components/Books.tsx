c6d61aaab0e0811b2d2241adb48891394a76d918
Elisa Bertoni <bertoni_elisa@hotmail.com>
2023-04-07 19:14:14 +1200

    add form on play component to add new book

--- client/components/Books.tsx
+++ client/components/Books.tsx
@@ -15,14 +15,15 @@ export default function Books() {
     dispatch(fetchBooks())
   }, [dispatch])
 
-  function handleChange(event: ChangeEvent<HTMLInputElement>) {
-    setNewBook({ title: event.target.value, author: event.target.value })
-  }
+  // function handleChange(event: ChangeEvent<HTMLInputElement>) {
+  //   setNewBook({ ...newBook, [event.target.name]: event.target.value })
+  // }
 
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
     dispatch(addBook(newBook))
-    setNewBook({title: '', author: ''})
+    setNewBook({ title: '', author: ''})
+    console.log(setNewBook)
   }
 
   return (
@@ -37,6 +38,31 @@ export default function Books() {
           </li>
         ))}
       </ul>
+
+      <form onSubmit={handleSubmit} aria-label='Add Book'>
+
+        <label htmlFor='bookTitle'>Book Title</label>
+        <input
+          type='text'
+          name='book title'
+          id='bookId'
+          placeholder='Title'
+          value={newBook.title}
+          onChange={(e) => setNewBook({...newBook, title: e.target.value})}
+        />
+
+        <label htmlFor='bookAuthor'>Book Author</label>
+        <input
+          type='text'
+          name='book author'
+          id='bookId'
+          placeholder='Author'
+          value={newBook.author}
+          onChange={(e) => setNewBook({...newBook, author: e.target.value})}
+        /> 
+
+      <button type='submit'>ADD NEW BOOK</button>
+      </form>
 
     </div>
   )
