b9c718ca923fbe1188da0df3109947a3bb84af7e
Elisa Bertoni <bertoni_elisa@hotmail.com>
2023-04-11 11:10:37 +1200

    add test for post route

--- server/routes/books.test.ts
+++ server/routes/books.test.ts
@@ -7,83 +7,81 @@ beforeEach(() => {
   jest.resetAllMocks()
 })
 
-afterAll(() => {
-  jest.restoreAllMocks()
+describe('GET /api/v1/books', () => {
+  it('should return data from the books table', async () => {
+    
+    const getBooksMockData = [
+      {
+        id: 1,
+        title: 'The Catcher in the Rye',
+        author: 'J.D. Salinger'
+      }
+    ]
+
+    // Arrange
+    expect.assertions(2)
+   
+    jest.mocked(db.getAllBooks).mockResolvedValue(getBooksMockData)
+
+    // Act
+    const response = await request(server).get('/api/v1/books')
+
+    // Assert
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual(getBooksMockData)
+  })
+
+  it('should return status 500 and an error message when database fails', async () => {
+    // Arrange
+    expect.assertions(1)
+  
+    jest.spyOn(console, 'log').mockImplementation(() => {})
+    jest.mocked(db.getAllBooks).mockRejectedValue(new Error('Mock error message'))
+  
+    // Act
+    const response = await request(server).get('/api/v1/books/')
+  
+    // Assert
+    expect(response.status).toBe(500)
+  })
 })
 
-const getBooksMockData = [
-  {
-    id: 1,
-    title: 'The Catcher in the Rye',
-    author: 'J.D. Salinger'
-  }
-]
+describe('POST /api/v1/books', () => {
+  it('should add data in the books table', async () => {
 
-// describe('GET /api/v1/books', () => {
-//   it('should return data from the books table', async () => {
-//     // Arrange
-//     expect.assertions(2)
+    const getAddBookMockData = [
+      {
+        id: 2,
+        title: 'Harry Potter',
+        author: 'J.K. Rowling'
+      }
+    ]
+    
+    // Arrange
+    expect.assertions(2)
 
-//     jest.mocked(db.getAllBooks).mockResolvedValue(getBooksMockData)
+    jest.mocked(db.addBook).mockResolvedValue(getAddBookMockData)
 
-//     // Act
-//     const response = await request(server).get('/api/v1/books')
+    // Act
+    const response = await request(server).post('/api/v1/books').send(getAddBookMockData)
 
-//     // Assert
-//     expect(response.status).toBe(200)
-//     expect(response.body).toEqual(getBooksMockData)
-//   })
+    // Assert
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual(getAddBookMockData)
+  })
 
-//   it('should return status 500 and an error message when database fails', async () => {
-//     // Arrange
-//     expect.assertions(1)
+  it('should return status 500 and an error message when database fails', async () => {
+    // Arrange
+    expect.assertions(1)
   
-//     jest.spyOn(console, 'log').mockImplementation(() => {})
-//     jest.mocked(db.getAllBooks).mockRejectedValue(new Error('Mock error message'))
+    jest.spyOn(console, 'log').mockImplementation(() => {})
+    jest.mocked(db.addBook).mockRejectedValue(new Error('Mock error message'))
   
-//     // Act
-//     const response = await request(server).get('/api/v1/books/')
+    // Act
+    const response = await request(server).post('/api/v1/books/')
   
-//     // Assert
-//     expect(response.status).toBe(500)
-//   })
-// })
-
-// const getAddBookMockData = [
-//   {
-//     id: 2,
-//     title: 'Harry Potter',
-//     author: 'J.K. Rowling'
-//   }
-// ]
-
-// describe('POST /api/v1/books', () => {
-//   it('should add data in the books table', async () => {
-//     // Arrange
-//     expect.assertions(2)
-
-//     jest.mocked(db.addBook).mockResolvedValue(getAddBookMockData)
-
-//     // Act
-//     const response = await request(server).post('/api/v1/books')
-
-//     // Assert
-//     expect(response.status).toBe(200)
-//     expect(response.body).toEqual(getBooksMockData)
-//   })
-
-//   it('should return status 500 and an error message when database fails', async () => {
-//     // Arrange
-//     expect.assertions(1)
-  
-//     jest.spyOn(console, 'log').mockImplementation(() => {})
-//     jest.mocked(db.getAllBooks).mockRejectedValue(new Error('Mock error message'))
-  
-//     // Act
-//     const response = await request(server).post('/api/v1/books/')
-  
-//     // Assert
-//     expect(response.status).toBe(500)
-//   })
+    // Assert
+    expect(response.status).toBe(500)
+  })
 })
 
