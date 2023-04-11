e5c15f2bbeeb3d12ce987d25f2a8c092ada03d79
Elisa Bertoni <bertoni_elisa@hotmail.com>
2023-04-11 15:44:44 +1200

    add test for deleting a book in the book component

--- client/components/Book.test.tsx
+++ client/components/Book.test.tsx
@@ -170,4 +170,113 @@ describe('Test Book Component', () => {
     expect(bookListItem).toBeVisible()
     expect(scope2.isDone()).toBeTruthy()
   })
+
+
+  it('should delete a book from the list', async () => {
+    const scope3 = nock('http://localhost')
+      .get('/api/v1/books')
+      .reply(200, [
+        { id: 1, title: 'The Catcher in the Rye', author: 'J.D.Salinger' },
+        { id: 2, title: 'It Ends with Us', author: 'Colleen Hoover' },
+        {
+          id: 3,
+          title: "Harry Potter and the Philosopher's Stone",
+          author: 'J.K.Rowling',
+        },
+        {
+          id: 4,
+          title: 'The Lion, the Witch, and the Wardrobe',
+          author: 'C.S. Lewis',
+        },
+        { id: 5, title: 'Normal People', author: 'Sally Rooney' },
+        { id: 6, title: 'The Duke and I', author: 'Julia Quinn' },
+        { id: 7, title: 'Girls That Invest', author: 'Simran Kaur' },
+        {
+          id: 8,
+          title: 'My Darling Lemon Thyme: Every Day',
+          author: 'Emma Galloway',
+        },
+        { id: 9, title: 'Search Inside Yourself', author: ' Chade-Meng Ta' },
+        {
+          id: 10,
+          title: 'Homegrown Kitchen: Everyday Recipes for Eating Well',
+          author: 'Nicola Galloway',
+        },
+        {
+          id: 11,
+          title:
+            'Healthy Baking: Nourishing breads, wholesome cakes, ancient grains and bubbling ferments',
+          author: 'Jordan Bourke',
+        },
+      ])
+
+    const bookId = 1
+    const scope4 = nock('http://localhost')
+    .delete(`/api/v1/books/${bookId}`)
+    .reply(200)
+
+    render(
+      <Router initialEntries={['/']}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </Router>
+    )
+
+    const booksList = await screen.findAllByRole('listitem')
+    await waitFor(() => expect(scope3.isDone()).toBeTruthy())
+
+    const scope5 = nock('http://localhost')
+      .get('/api/v1/books')
+      .reply(200, [
+        { id: 2, title: 'It Ends with Us', author: 'Colleen Hoover' },
+        {
+          id: 3,
+          title: "Harry Potter and the Philosopher's Stone",
+          author: 'J.K.Rowling',
+        },
+        {
+          id: 4,
+          title: 'The Lion, the Witch, and the Wardrobe',
+          author: 'C.S. Lewis',
+        },
+        { id: 5, title: 'Normal People', author: 'Sally Rooney' },
+        { id: 6, title: 'The Duke and I', author: 'Julia Quinn' },
+        { id: 7, title: 'Girls That Invest', author: 'Simran Kaur' },
+        {
+          id: 8,
+          title: 'My Darling Lemon Thyme: Every Day',
+          author: 'Emma Galloway',
+        },
+        { id: 9, title: 'Search Inside Yourself', author: ' Chade-Meng Ta' },
+        {
+          id: 10,
+          title: 'Homegrown Kitchen: Everyday Recipes for Eating Well',
+          author: 'Nicola Galloway',
+        },
+        {
+          id: 11,
+          title:
+            'Healthy Baking: Nourishing breads, wholesome cakes, ancient grains and bubbling ferments',
+          author: 'Jordan Bourke',
+        },
+        {
+          id: 12,
+          title: 'The Kite Runner',
+          author: 'Khaled Hosseini',
+        },
+      ])
+
+    const deleteButton = within(booksList[0]).getByRole('button', { name: 'Delete' })
+
+    act(() => {
+      userEvent.click(deleteButton)
+    })
+
+    await waitFor(() => expect(scope4.isDone()).toBeTruthy())
+    await waitFor(() => expect(scope5.isDone()).toBeTruthy())
+
+    const newBooksList = await screen.findAllByRole('listitem')
+    expect(newBooksList).toHaveLength(11)
+  })
 })
